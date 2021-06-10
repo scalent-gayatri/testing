cd1e6b95f35bec71d102164ff10a832790c8db99
gayatri chougale <gayatri61995@gmail.com>
2021-06-10 10:32:21 +0530

    remove comments

--- user/user.go
+++ user/user.go
@@ -1,8 +1,6 @@
 package user
 
 import (
-	"fmt"
-
 	"github.com/scalent-gayatri/testing/doer"
 )
 
@@ -11,6 +9,5 @@ type User struct {
 }
 
 func (u *User) Use() error {
-	fmt.Println("Inside Use() ******")
 	return u.Doer.DoSomething(123, "Hello GoMock")
 }
